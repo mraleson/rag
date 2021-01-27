05ff8e7ccebb4fe75950807bb2eafe27c37dbb0b
mraleson <markraleson@outlook.com>
2021-01-26 23:09:04 -0700

    Set RAG_ENV to development when running dev server

--- rag/cli/commands/serve.py
+++ rag/cli/commands/serve.py
@@ -1,6 +1,15 @@
+import os
 from channels.management.commands.runserver import Command as RunServerCommand
 from rag.cli.command  import DjangoCommand
+
 
 class Command(DjangoCommand, RunServerCommand):
     default_addr = '0.0.0.0'
     default_addr_ipv6 = '::'
+
+    def __init__(self, *args, **kwargs):
+        os.environ['RAG_ENV'] = os.environ.get('RAG_ENV', 'development')
+        super().__init__(*args, **kwargs)
+
+    def handle(self, *args, **kwargs):
+        super().handle(*args, **kwargs)
