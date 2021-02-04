51e47799bd7f3dde60d2f0e6c3e9c00d09b2053e
mraleson <markraleson@outlook.com>
2021-02-03 18:34:58 -0700

    Fix bug where RAG_ENV was set on all commands not just dev server

--- rag/cli/commands/serve.py
+++ rag/cli/commands/serve.py
@@ -7,9 +7,6 @@ class Command(DjangoCommand, RunServerCommand):
     default_addr = '0.0.0.0'
     default_addr_ipv6 = '::'
 
-    def __init__(self, *args, **kwargs):
+    def execute(self, *args, **kwargs):
         os.environ['RAG_ENV'] = os.environ.get('RAG_ENV', 'development')
-        super().__init__(*args, **kwargs)
-
-    def handle(self, *args, **kwargs):
-        super().handle(*args, **kwargs)
+        super().execute(*args, **kwargs)
