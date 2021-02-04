504469f7307deee70b2cdc759c076410a48d6f33
mraleson <markraleson@outlook.com>
2021-02-03 18:34:06 -0700

    Set RAG_ENV to test if unset when running tests

--- rag/cli/commands/create/templates/large/settings/__init__.py
+++ rag/cli/commands/create/templates/large/settings/__init__.py
@@ -5,5 +5,5 @@ ENVIRONMENT = os.environ.get('RAG_ENV', 'production').lower()
 from .production import *
 if ENVIRONMENT == 'development':
     from .development import *
-if ENVIRONMENT == 'testing':
+if ENVIRONMENT == 'test':
     from .testing import *
