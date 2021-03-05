7e2137318ccecd20c698b6398b9c29f6bc0f36b6
mraleson <markraleson@outlook.com>
2021-03-04 17:43:34 -0700

    Update application to align better with django configuration and models

--- rag/core/application.py
+++ rag/core/application.py
@@ -1,70 +1,52 @@
 import os
-from inspect import isclass
-from functools import wraps
-from types import ModuleType
-from importlib import import_module
+import importlib
 from django.apps import apps
-from django.apps import AppConfig
 from django.conf import settings
 from django.urls import set_script_prefix
-from django.db.models.base import Model as DjangoModel
 from django.utils.log import configure_logging
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from rag import rest
-from rag.core.models import Model
 from rag.core.settings import default
+from rag.core.utils import find_asgi_path
 
 
 class Urls:
-    def __init__(self, urlpatterns, restpatterns):
-        self.urlpatterns = urlpatterns
-        self.restpatterns = restpatterns
+    def __init__(self, source):
+        self.source = source or []
+        self.urlpatterns = []
+        self.restpatterns = []
+
+    def add(self, urls):
+        if not isinstance(urls, list):
+            urls = [urls]
+        self.restpatterns.extend(urls)
+
+    def setup(self):
+        if isinstance(self.source, str):
+            if ':' in self.source:
+                module, attribute = self.source.split(':')
+            else:
+                module, attribute = [self.source, 'urls']
+            urls = getattr(importlib.import_module(module), attribute)
+            self.add(urls)
+        else:
+            self.add(self.source)
 
 class Application:
 
-    def __init__(self, name, settings=None, urls=None, models=None, migrations=None):
-        self.name = name
+    def __init__(self, application, settings=None, urls=None, app=None):
+        self.app = application
         self.settings = settings
-        self.migrations = migrations
-        self.urls = self.url_config(urls)
-        self.app = self.app_config(name)
+        self.urls = Urls(urls)
         self.setup()
-        self.models = self.model_config(models)
-
-    @staticmethod
-    def url_config(urls):
-        if not urls: urls = []
-        if isinstance(urls, str):
-            return import_module(urls)
-        else:
-            return Urls([], urls)
-
-    def model_config(self, models):
-        if not models: return []
-        if isinstance(models, ModuleType):
-            models = [getattr(models, k) for k in dir(models) if
-                not k.startswith('_')
-                and isclass(getattr(models, k))
-                and (issubclass(getattr(models, k), Model) or issubclass(getattr(models, k), DjangoModel))]
-        for model in models:
-            model.finalize(self.app.label)
-        return models
-
-    @staticmethod
-    def app_config(name):
-        return AppConfig(name, import_module(name))
 
     def route(self, route, method, *args, **kwargs):
         def decorator(func):
-            self.urls.restpatterns.append(rest(route, method, func, *args, **kwargs))
+            self.urls.add(rest(route, method, func, *args, **kwargs))
+            func.routed = True
             return func
         return decorator
-
-    def register(self, model):
-        self.models.append(model)
-        model.finalize(self.app.label)
-        return model
 
     def setup(self, set_prefix=True):
         # don't allow DJANGO_SETTINGS_MODULE because it loads settings differently than settings.configure (see dj docs)
@@ -73,21 +55,22 @@ class Application:
 
         # load settings
         if isinstance(self.settings, str):
-            self.settings = import_module(self.settings)
-        if isinstance(self.settings, ModuleType):
-            module = self.settings
+            module = importlib.import_module(self.settings)
             self.settings = {k: getattr(module, k) for k in dir(module) if not k.startswith('_') and k.isupper()}
 
         # inject urls into settings
         self.settings['ROOT_URLCONF'] = self.urls
 
         # inject root asgi app setting
-        self.settings['ASGI_APPLICATION'] = f'{self.name}:app.router'
+        if 'ASGI_APPLICATION' not in self.settings:
+            self.settings['ASGI_APPLICATION'] = find_asgi_path()
 
-        # inject migrations module
-        if self.migrations and 'MIGRATIONS_MODULES' not in self.settings:
-            self.settings['MIGRATION_MODULES'] = {self.app.label: self.migrations.__name__}
+        # set default migrations module
+        if 'MIGRATIONS_MODULES' not in self.settings and importlib.util.find_spec('migrations'):
+            self.settings['MIGRATION_MODULES'] = {self.app: 'migrations'}
 
+        # add this application to installed apps
+        self.settings['INSTALLED_APPS'] = [self.app] + default.INSTALLED_APPS
 
         # configure settings
         settings.configure(default, **self.settings)
@@ -100,7 +83,10 @@ class Application:
             set_script_prefix('/' if settings.FORCE_SCRIPT_NAME is None else settings.FORCE_SCRIPT_NAME)
 
         # populate apps
-        apps.populate([self.app] + settings.INSTALLED_APPS)
+        apps.populate(settings.INSTALLED_APPS)
+
+        # setup urls
+        self.urls.setup()
 
     @property
     def router(self):
